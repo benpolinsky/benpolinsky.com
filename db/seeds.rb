42fd933fea6e58fd28eff15b7790cb5fe5ae4e97
Ben Polinsky <benjamin.polinsky@gmail.com>
2016-08-17 14:30:53 -0400

    add link

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 # Example Projects
-Project.delete_all
-Link.delete_all
+
 Project.create({
   name: "Philadelphia Slick",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
